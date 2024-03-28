8b03c165a73b8764cc4c0cdeec707c2677d42680
Mres <tu8fme6jw@code.edu.az>
2024-03-28 13:28:09 +0400

    task

---
+++ Week-5/js/main.js
@@ -0,0 +1,252 @@
+// var verilenSaniye = Number(prompt("Nece saniye oldugunu tapmaq istediyiniz ededi daxil edin"))
+//         verilenSaniye = parseInt(verilenSaniye);
+//         var deqiqe = Math.floor=(verilenSaniye / 60)
+//         var saniye = verilenSaniye % 60
+//         console.log(verilenSaniye = + deqiqe + "deqiqedir");
+
+
+// var verilenTemperatur = prompt("nece selsi oldugunu tapmaq istediyiniz temperaturu yazin");
+// verilenTemperatur = parseInt (verilenTemperatur);
+// var Selsi = verilenTemperatur * 5 / 9;
+// alert(Selsi + "derecedir");
+
+//    var eded1=Number(prompt("ededi daxil edin"))
+//     var eded2=Number(prompt("ededi daxil edin"))
+//     var eded3=Number(prompt("ededi daxil edin"))
+//     var eded4=Number(prompt("ededi daxil edin"))
+//     var eded5=Number(prompt("ededi daxil edin"))
+//     eded1=parseInt(eded1)
+//     eded2=parseInt(eded2)
+//     eded3=parseInt(eded3)
+//     eded4=parseInt(eded4)
+//     eded5=parseInt(eded5)
+//     var edediorta= (eded1+eded2+eded3+eded4+eded5)/5
+//     alert(edediorta)
+
+// var verilenYas = Number(prompt("Dogum iliniz"))
+// verilenYas = parseInt(verilenYas);
+// var indiki = 2024- verilenYas;
+// alert(indiki + "yasiniz var")
+
+// var verilenEded = Number(prompt("faizini tapmaq istediyiniz eded"))
+// var verilenFaiz = Number(prompt("faizi yazin"))
+// verilenEded=parseInt(verilenEded);
+// verilenFaiz=parseInt(verilenFaiz);
+// var son= verilenEded * verilenFaiz /100;
+// alert(son)
+
+// var verilenKatet = Number(prompt("kateti daxil edin"))
+// var verilenHip = Number(prompt("hipotenuzu daxil edin"))
+// verilenKatet = parseInt (verilenKatet);
+// verilenHip= parseInt (verilenHip);
+// var sahe= verilenHip * verilenKatet / 2;
+// alert(sahe)
+
+// var verilenMiqdar = Number(prompt("istediyiniz miqdari daxil edin"))
+// var verilenFaiz = Number(prompt("faizini yazin"))
+// var verilenAy =  Number(prompt("nece ayliq istediyinizi yazin"))
+// verilenAy=parseInt(verilenAy);
+// verilenFaiz=parseInt(verilenFaiz);
+// verilenMiqdar=parseInt(verilenMiqdar);
+// var yekun = (verilenMiqdar +(verilenMiqdar * verilenFaiz/100)) / verilenAy;
+// alert(yekun)
+
+// var ad = prompt("adinizi daxil edin")
+// var soyad = prompt ("soyadinizi daxil edin")
+// console.log(ad + soyad + "salam");
+
+//   var teref = prompt("kvadratin terefini yazin")
+//   teref=parseInt(teref);
+//   var perimetr= teref * 4;
+//   var sahe= teref * teref;
+//   alert("Perimetr=" + perimetr )
+//   alert("Sahe=" + sahe )
+
+
+// var eded1=Number(prompt("1ci ededi daxil edin"))
+// var eded2=Number(prompt("2ci ededi daxil edin"))
+// eded1=parseInt(eded1);
+// eded2=parseInt(eded2);
+// var kubuncemi= (eded1 **3)+(eded2**3);
+// var ceminkubu = (eded1 + eded2)**3;
+// alert(ceminkubu)
+// alert(kubuncemi)
+
+//     var yas=Number(prompt("yasinizi daxil edin"))
+//     if(yas<0)
+//     alert("yasinizi duzgun daxil edin")
+//     if(yas>65)
+//     alert("siz pensiya yasindasiniz")
+// else(0<yas<=65)
+// alert("siz pensiya yasinda deylsiz")
+
+
+//     var eded1=Number(prompt("1ci ededi daxil edin"))
+//     var eded2=Number(prompt("2ci ededi daxil edin"))
+//     var eded3=Number(prompt("3ci ededi daxil edin"))
+//    if(eded1===eded2 || eded2===eded3 || eded1===eded3){
+//        alert("beraberyanli ucbucaq")
+//    }
+
+// let x =Number(prompt("ededi daxil edin"))
+// if(x%7==0){
+//     alert("eded 7e bolunur")
+// }
+// else {
+//     let a=x%7
+//     if( a<4){
+//          alert((x-a)/7)
+//     }
+// }
+
+// let num=Number(prompt("ededi daxil edin"))
+// for (let i=2; i<=0; i++){
+//     if(x%1==0 ){
+//         counter++;
+//     }
+// }
+// if(counter>2){
+//     alert("murekkebdir");
+// }
+// else{
+//     alert("eded sadedir");
+// }
+
+
+
+
+// var eded1 = Number(prompt("1ci ededi daxil edin"))
+// var eded2 = Number(prompt("2ci ededi daxil edin"))
+// var eded3 = Number(prompt("3ci ededi daxil edin"))
+// if (eded1 === eded2 || eded2 === eded3 || eded1 === eded3) {
+//     alert("beraberyanli ucbucaq")
+// }
+
+// else (eded1===eded2===eded3)
+//     alert("beraberterefli ucbucaq")
+
+// for(let i=0; i<5;i++){
+//     console.log("hemid")
+// }
+
+// let nomre=prompt("eded daxiledin");
+// nomre=parseInt(nomre);
+// if(nomre % 2 ===0) {
+//     alert("eded cutdur")
+// }
+// else{
+//     alert("eded tekdir")
+// }
+
+// if (nomre >= 1 && nomre <= 12) {
+//     let ay:
+//         switch (nomre) {
+//         case 1
+//             ay = "yanvar";
+//             break;
+//         case 2
+//             ay = "fevral";
+//             break;
+//         case 3
+//             ay = "mart"
+//             break;
+//         case 4
+//             ay = "aprel"
+//             break;
+//         case 5
+//             ay = "may"
+//             break;
+//         case 6
+//             ay = "iyun"
+//             break;
+//         case 7
+//             ay = "iyul"
+//             break;
+//         case 8
+//             ay = "avqust"
+//             break;
+//         case 9
+//             ay = "sentyabr"
+//             break;
+//         case 10
+//             ay = "oktyabr"
+//             break;
+//         case 11
+//             ay = "noyabr"
+//             break;
+//             case 12
+//             ay = "dekabr"
+//             break;
+//         default:
+//             ay = "bu reqemde ay yoxdur";
+
+// for(let i=0; i<5;i++){
+//         console.log("hemid")
+//     }
+    
+// for(let i=20; i>=1; i--) {
+//     console.log(i)
+// }
+
+// const cities = ["Baki","Gence","Sumqayit"];
+// console.log()
+// for (const city of cities)
+// console.log(city)
+
+// const cities = ["Baki","Gence","Sumqayit"];
+// let index=0
+// for (const element of cities);
+// console.log("Index:" + index + element);index++;
+
+// var reqem=[1,4,5,6,7,8,34,56,98];
+// var toplam= 0;
+// for (var i = 0; i <reqem.length;i++){
+//     toplam+=reqem[i];
+// }
+// console.log(toplam)
+
+
+// for  (var i= 1;i <=50;i++){
+//     console.log(i)
+// }
+
+
+// for  (var i= 0;i <=50;i+=3){
+//     console.log(i)
+// }
+
+
+// for  (var i= 2;i <=50;i+=2){
+//     console.log(i)
+// }
+
+
+// for  (var i= 8;i <=50;i+=8){
+//     console.log(i)
+// }
+
+
+// var toplam= 0;
+// for (var i = 1; i <= 100;i++){
+//     toplam+=i;
+// }
+// console.log(toplam)
+
+// var reqem=parseInt(prompt("faktorialini tapmaq istediyiniz eded"));
+// var faktorial=1;
+// for(var i=1;i<=reqem;i++){
+//     faktorial *=i
+// }
+// alert(faktorial)
+
+
+for (var i = 1; i <= 100;i++){
+    if (i % 3===0 && i % 5===0)
+    console.log("FizzBuzz");
+
+else if(i % 3===0)
+console.log("Fizz");
+else if(i % 5===0)
+console.log("Buzz");
+else(i)
+}
